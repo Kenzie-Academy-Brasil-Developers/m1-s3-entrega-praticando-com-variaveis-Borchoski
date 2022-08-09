ede6783e66f84676372c6df8e647d93b008827aa
Joao Borchoski <joaohenriqueborchoski>
2022-08-09 17:17:24 -0300

    Entrega

---
+++ script.js
@@ -0,0 +1,18 @@
+let nome = ("Jhon")
+let sobrenome = ("Doyle")
+let anoNascimento = 1991
+let cidade = ("Curitiba")
+let anoAtual = 2022
+let idade = (anoAtual-anoNascimento)
+console.log("Eu sou o "+nome+" "+sobrenome+ " tenho "+(anoAtual-anoNascimento)+" anos e moro em "+cidade+".")
+
+
+
+
+
+
+
+
+
+
+//"Eu sou o Jhon Doyle tenho 31 anos e moro em Curitiba"
